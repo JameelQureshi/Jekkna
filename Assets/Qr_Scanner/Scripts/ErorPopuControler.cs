ed50f9c082eb2334f272bf03eafdbf5e5014a044
SufyanQureshi <sufyannqureshi1998@gmail.com>
2021-05-24 10:51:23 +0500

    APK Working

---
+++ Assets/Qr_Scanner/Scripts/ErorPopuControler.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public GameObject Errorpopup;
+    private void Update()
+    {
+        if (Application.internetReachability != NetworkReachability.NotReachable)
+        {
+            Errorpopup.SetActive(false);
+        }
+        else
+
+        {
+            Errorpopup.SetActive(true);
+        }
+    }
+    
+}
