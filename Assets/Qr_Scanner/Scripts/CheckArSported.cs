daf450c06bd6a0164c847137865de7fe2de19607
SufyanQureshi <sufyannqureshi1998@gmail.com>
2021-06-11 20:49:26 +0500

    UI Ubdate

---
+++ Assets/Qr_Scanner/Scripts/CheckArSported.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.XR.ARFoundation;
+namespace ARSupportCheck
+{ 
+    public class CheckArSported : MonoBehaviour
+    {
+        public UnityEvent CheckArSport;
+
+        public void Start()
+        {
+            if (!ARSupportChecker.IsSupported())
+            {
+                CheckArSport.Invoke();
+            }
+            
+        }
+    }
+}
